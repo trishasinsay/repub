863aaa54abcac2f4f77e517300621ee9f7807505
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-15 20:04:05 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Main from "../components/Main";
 
-import { useState, useEffect } from "react";
-
-export default function Home() {
-  //
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
-
-  //
-  useEffect(() => {
-    setLoading(true);
-    fetch("https://repub-aeej.vercel.app/")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
+export default function repub() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
