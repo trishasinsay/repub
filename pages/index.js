6c3bd8bb6014284a5f9c65e21cdf49f031d111fc
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-15 16:28:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   //
   useEffect(() => {
     setLoading(true);
-    fetch("localhost:3000/repub")
+    fetch("http://localhost:3000/repub")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
